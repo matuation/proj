05b108b22485df1f8411e4c2efbbf47627123474
matuation <git.user>
2025-08-28 17:43:14 +0300

    добавили функцию

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,7 +3,13 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("---");
         System.out.println("12223");
-        System.out.println("12223");
+    }
+
+    public static void doit(String[] args) {
+        System.out.println("He111llo World!");
+        System.out.println("12111223");
+        System.out.println("12111223");
     }
 }
