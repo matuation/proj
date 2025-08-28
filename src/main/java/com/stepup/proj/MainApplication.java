1b09597f1de6e180c5bda11f0533c41bd2ce7bf5
matuation <git.user>
2025-08-28 17:32:02 +0300

    доработка логики проекта и коробка

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("123");
+        System.out.println("12223");
     }
 }
