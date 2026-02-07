adb608a0b2ed9e8d47ecf043f48851f7d1cc16f8
Alexey Schastlivtsev <matuation@gmail.com>
2026-02-07 21:26:06 +0300

    добавлен класс массивов

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,6 +2,14 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-
+        Dot dot1 = new Dot(1,3);
+        Dot dot2 = new Dot(1,3);
+        Dot dot3 = new Dot(5,8);
+        System.out.println("Dot1 is= "+dot1);
+        System.out.println("Dot2 is= "+dot2);
+        System.out.println("Dot3 is= "+dot3);
+        System.out.println(dot1==dot2);
+        System.out.println(dot1==dot3);
+        System.out.println(dot2==dot3);
     }
 }
