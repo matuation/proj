ad348498c22cc22dbc13480cb59e28fc02cccf12
Alexey Schastlivtsev <matuation@gmail.com>
2026-02-19 22:57:08 +0300

    добавили клонирование точке

--- src/main/java/ru/courses/main/MainApplication.java
+++ src/main/java/ru/courses/main/MainApplication.java
@@ -4,9 +4,16 @@ import ru.courses.geometry.Point;
 
 public class MainApplication {
 
-    public static void main(String[] args) {
-        Point point = Point.ofCoordinates(1, 3);
+    public static void main(String[] args) throws CloneNotSupportedException {
+        //Points
+        System.out.println("==========Points============");
+        Point point1 = Point.ofCoordinates(1, 3);
         Point point2 = Point.ofCoordinates(1, 3);
-        System.out.println(point.equals(point2));
+        System.out.println(point1.equals(point2));
+        System.out.println(point1==point2);
+        Point point3 = point1.clone();
+        System.out.println(point1.equals(point3));
+        System.out.println(point1==point3);
+        System.out.println("============================");
     }
 }
